d53b418e50e3d3e7f122dadb55aadaae029c656c
Divani David Fuentes Marcos <divani@bambut.mx>
2026-06-23 20:54:34 -0600

    update image route

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL,
+  site: process.env.SITE_URL || process.env.DEPLOY_PRIME_URL || process.env.URL || 'https://xv-nicole-test.netlify.app',
   vite: {
     plugins: [tailwindcss()]
   },
